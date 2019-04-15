3883a20fdbc8bd60c06cdf45aa2d22764919ce37
Vaidios <25388414+Vaidios@users.noreply.github.com>
2019-04-15 22:56:31 +0200

    Initial commit

---
+++ numericalAnalysis/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  numericalAnalysis
+//
+//  Created by Kamil Sosna on 15/04/2019.
+//  Copyright © 2019 Kamil Sosna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
